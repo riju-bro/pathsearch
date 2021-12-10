51e53c3e4d1e5346fbf85599907fa11ef2cdf075
MuhammedRijnas <muhammedrijnas2004@gmail.com>
2021-12-10 21:38:45 +0530

    wordlists and url for victim addresses added in argument.py

---
+++ argument.py
@@ -0,0 +1,9 @@
+class Argument:
+    def __init__(self):
+        from argparse import ArgumentParser
+        argument_parser = ArgumentParser(f'python3 {__file__}')
+        # argument for url
+        argument_parser.add_argument('url', action='store', help='url of the victim')
+        # argument for wordlists path
+        argument_parser.add_argument('-w', '--wordlist', action='store', help='wordlists path', required=True)
+        self.args = argument_parser.parse_args()
