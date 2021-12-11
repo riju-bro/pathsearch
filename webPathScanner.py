c777bcd56cbf27f9debabcc1ae731f9e5843ff33
MuhammedRijnas <muhammedrijnas2004@gmail.com>
2021-12-11 08:50:37 +0530

    dirsearch function called using thread in __main__ function

--- webPathScanner.py
+++ webPathScanner.py
@@ -1,9 +1,11 @@
 from argument import Argument
+from threading import Thread
 
 argument_parser = Argument()
 # assigning arguments to args
 args = argument_parser.args
 url = args.url.rstrip('/')
+thread = args.thread
 
 
 def __main__():
@@ -11,6 +13,17 @@ def __main__():
     if not os.path.isfile(args.wordlist):
         print('The wordlist does not exist')
         exit(-1)
+    wordlist = open(args.wordlist, 'r')
+    paths = wordlist.readlines()
+    j = 0  # start index
+    diff = int(len(paths)/thread)
+    for i in range(diff, len(paths), diff):
+        t = Thread(target=dirsearch, args=(paths[j:i], ))
+        t.start()
+        j = i
+    if i != len(paths) - 1:
+        t = Thread(target=dirsearch, args=(paths[j:len(paths)], ))
+        t.start()
 
 
 def dirsearch(paths):
